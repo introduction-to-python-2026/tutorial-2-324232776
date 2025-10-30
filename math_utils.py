08830f34d1887109f4001bbfbf44658ca81370ae
324232776 <ledoronlevi@gmail.com>
2025-10-30 13:37:35 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,2 +1,12 @@
-def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+
+ def find_max_number(num1, num2, num3):
+    if num1 == num2 == num3:
+      print('even steven')
+    elif num1 >= num2 and num1 >= num3:
+      return num1 
+    elif num2 >=  num1 and num2 >= num3:
+      return num2
+    else :
+      return num3 
+
+         # Replace 'pass' with code
